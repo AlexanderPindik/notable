3939403e82ff55ec4ca5213c55a730a69f62fc4a
AlexanderPindik <alexander.pindik@nevafilm.ru>
2018-09-27 12:45:43 +0300

    исправлена ошибка из-за несовместимости версий монго 2 и 3

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
-const express        = require('express');
-const MongoClient    = require('mongodb').MongoClient;
-const bodyParser     = require('body-parser');
-const db             = require('./config/db');
-const app            = express();
-const port = 8000;
-app.use(bodyParser.urlencoded({ extended: true }));
-MongoClient.connect(db.url, (err, database) => {
-  if (err) return console.log(err)
-  require('./app/routes')(app, database);
-  app.listen(port, () => {
-    console.log('We are live on ' + port);
-  });               
+const express       = require('express');
+const MongoClient   = require('mongodb').MongoClient;
+const bodyParser    = require('body-parser');
+const db            = require('./config/db');
+const app           = express();
+const port          = 8000;
+
+app.use(bodyParser.urlencoded({ extended:true }))
+MongoClient.connect(db.url, { useNewUrlParser: true },  (err, client)=>{
+    var db = client.db('notable');
+    if (err) return console.log(err)
+
+    require('./app/routes')(app, client);
+    app.listen(port,()=>{
+        console.log('we are live at '+ port);
+    });
 })
--- temp.js
+++ temp.js
@@ -1,7 +1,4 @@
-var ObjectId = require('mongodb').ObjectID;
- module.exports= function (app, client) {
-        var db = client.db('notable');
-        //find One
+//find One
         app.get('/notes/:id', (req, res)=>{
                 const id =req.params.id;
                 const details ={'_id': new ObjectId(id)}
@@ -48,19 +45,3 @@ var ObjectId = require('mongodb').ObjectID;
                     }
                 });
             });
-            //insert route
-            app.post('/notes', (req, res)=>{
-                const note ={text: req.body.body, title: req.body.title};
-                db.collection('notes').insert(note, (err, results)=>{
-                    if(err)
-                    {
-                        res.send({'error':"An error has occured"})
-                    }
-                    else
-                    {
-                        res.send(results.ops[0])
-                    }
-                });
-
-            });
-        };
